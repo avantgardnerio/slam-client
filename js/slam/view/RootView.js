b89a66fa0dca8d2d65f512e8db4bc399222598eb
Brent Gardner <brent.gardner@zenplanner.com>
2014-12-27 15:57:25 -0700

    Drawing floorplan

--- js/slam/view/RootView.js
+++ js/slam/view/RootView.js
@@ -6,16 +6,48 @@ define([
     var RootView = function() {
         var self = {};
 
-        var cnvMain = $('<canvas/>');
+        var PX_PER_FT = 40; // TODO: Un hard code
+
+        var cnvMain;
+        var ctx;
+        var background;
+
+        var init = function() {
+            cnvMain = $('<canvas/>')[0];
+            ctx = cnvMain.getContext("2d");
+
+            background = $('<img/>')[0];
+            $(background).ready(function() { self.invalidate(); });
+            $(background).attr('src', 'img/floor_plan_example.png'); // TODO: Un hard code
+
+            $(window).resize(onResize);
+            onResize();
+        };
 
         self.getElement = function() {
             return cnvMain;
         };
 
         self.onLoad = function() {
-
+            self.invalidate();
         };
 
+        self.invalidate = function() {
+            window.requestAnimationFrame(draw);
+        };
+
+
+        var onResize = function() {
+            $(cnvMain).attr('width', $(window).innerWidth());
+            $(cnvMain).attr('height', $(window).innerHeight());
+            self.invalidate();
+        };
+
+        var draw = function() {
+            ctx.drawImage(background, 0, 0, background.width, background.height);
+        };
+
+        init();
         return self;
     };
 
