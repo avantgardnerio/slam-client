0e185c5a5e7aca12a123b1b6041cd89b47d14e80
Brent Gardner <brent.gardner@zenplanner.com>
2015-01-03 09:20:12 -0700

    Finding map pixels

--- js/slam/view/RootView.js
+++ js/slam/view/RootView.js
@@ -7,6 +7,7 @@ define([
         var self = {};
 
         var PX_PER_FT = 40; // TODO: Un hard code
+        var IN_PER_FT = 12;
 
         var cnvMain;
         var ctx;
@@ -48,7 +49,7 @@ define([
             if(background.width > 0 && background.height > 0) {
                 ctx.drawImage(background, 0, 0, background.width, background.height);
                 if(bitmap === undefined) {
-                    parseMap();
+                    setTimeout(parseMap, 1);
                 }
             }
         };
@@ -58,9 +59,19 @@ define([
             var imgData = ctx.getImageData(0, 0, background.width, background.height).data;
             for(var y = 0; y < background.height; y++) {
                 for(var x = 0; x < background.width; x++) {
-                    var i = y * background.height * 3 + x * 3;
-                    var hasWall = imgData[i + 0] < 255 || imgData[i + 0] < 255 || imgData[i + 0] < 255;
-                    bitmap[y * background.height + x] = hasWall;
+                    var i = y * background.width * 4 + x * 4;
+                    var hasWall = imgData[i + 0] < 50 && imgData[i + 1] < 50 && imgData[i + 2] < 50;
+                    bitmap[y * background.width + x] = hasWall;
+                }
+            }
+
+            ctx.fillStyle = '#0000FF';
+            for(var y = 0; y < background.height; y++) {
+                for(var x = 0; x < background.width; x++) {
+                    var hasWall = bitmap[y * background.width + x];
+                    if(hasWall) {
+                        ctx.fillRect(x, y, 1, 1);
+                    }
                 }
             }
         };
